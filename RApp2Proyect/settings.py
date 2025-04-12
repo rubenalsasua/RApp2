77f024bfaa024a60803d4629ccced2534f99dad0
rubenalsasua <r.alsasua@opendeusto.es>
2025-04-12 21:45:51 +0200

    Para static

--- RApp2Proyect/settings.py
+++ RApp2Proyect/settings.py
@@ -113,6 +113,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = '/home/rubenalsasua/RApp2Proyect/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
